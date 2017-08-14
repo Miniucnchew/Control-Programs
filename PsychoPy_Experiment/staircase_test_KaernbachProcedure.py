b0771d500f29156658ef5e1086b2002a15dfc73a
Miniucnchew <ods7@pitt.edu>
2017-08-14 12:43:18 -0400

    Finally got the kaernbach procedure working. I'm still unsure of my step sizes, but it produces results.

--- PsychoPy_Experiment/staircase_test_KaernbachProcedure.py
+++ PsychoPy_Experiment/staircase_test_KaernbachProcedure.py
@@ -9,7 +9,7 @@ from subprocess import call
 try:#try to get a previous parameters file
     expInfo = fromFile('TestData/lastParams.pickle')
 except:#if not there then use a default set
-    expInfo = {'observer':'ods7', 'waveform':1, 'frequency':20, 'starting amplitude':1000, 'offset':0, 'duration':5}
+    expInfo = {'observer':'ods7', 'waveform':1, 'frequency':20, 'starting amplitude':300, 'offset':0, 'duration':5, 'max reversals':6}
 expInfo['dateStr']= data.getDateStr() #add the current time
 #present a dialogue to change params
 dlg = gui.DlgFromDict(expInfo, title='JND Exp', fixed=['dateStr'])
@@ -24,33 +24,35 @@ dataFile = open(fileName+'.csv', 'w')#a simple text file with 'comma-separated-v
 dataFile.write('new_amp,user_resp, resp_correct\n')
 
 #create window and stimuli
-win = visual.Window([800,600],allowGUI=True, monitor='testMonitor', units='cm', fullscr=True)
+win = visual.Window([1600,1200],allowGUI=False, monitor='testMonitor', units='cm', fullscr=True)
+win.mouseVisible = False
 #and some handy clocks to keep track of time
 globalClock = core.Clock()
 trialClock = core.Clock()
 
 #display instructions and wait
-message1 = visual.TextStim(win, pos=[0,+3], text = 'This experiment will measure your ability to notice a change in the provided texture simulation.')
-message2= visual.TextStim(win, pos=[0,-3],
-    text="Press the left arrow key if the two stimuli are identical," + 
-    " and the right arrow key if they are different. If you are unsure, press the down arrow key.")
-message3 = visual.TextStim(win, pos=[-2, -7], text='Hit a key when ready.')
+message1 = visual.TextStim(win, pos=[0,+3], text = 'This experiment will measure your ability to discriminate between two texture simulations.')
+#message2= visual.TextStim(win, pos=[0,-3],
+#    text="Press the left arrow key if the first textureis stronger," + 
+#    " and the right arrow key if the second texture is stronger. If you are unsure, press the down arrow key.")
+message3 = visual.TextStim(win, pos=[-2, -7], text='Hit any key to continue.')
 message1.draw()
-message2.draw()
+#message2.draw()
 message3.draw()
 #fixation.draw()
 win.flip()#to show our newly drawn 'stimuli'
 #pause until there's a keypress
 event.waitKeys()
 
-message_stim1_wait = visual.TextStim(win, text = 'Stimulus 1 will begin in 1 second')
-message_stim1 = visual.TextStim(win, text = 'Stimulus 1')
-message_stim2_wait = visual.TextStim(win, text = 'Stimulus 2 will begin in 1 second')
-message_stim2 = visual.TextStim(win, text = 'Stimulus 2')
-message_response = visual.TextStim(win, text = 'If the first stimulus was stronger, press the left arrow key'
-    +' ,if the second stimulus was stronger, press the right arrow key.'
+message_stim1_wait = visual.TextStim(win, text = 'Texture 1 will begin in 1 second')
+message_stim1 = visual.TextStim(win, text = 'Texture 1')
+message_stim2_wait = visual.TextStim(win, text = 'Texture 2 will begin in 1 second')
+message_stim2 = visual.TextStim(win, text = 'Texture 2')
+message_response = visual.TextStim(win, text = 'If the first texture was stronger, press the left arrow key'
+    +' ,if the second texture was stronger, press the right arrow key.'
     +' If you are unsure of which stimulus is stronger, press the down arrow key. \n\n'
     +'If you would like to replay one of the stimuli, press the number key corresponding to the stimulus you would like to return to.')
+image_response = visual.ImageStim(win, image = 'PsychExperiment_KeyboardInstructions.png', units = 'pix', interpolate = True, size = [1440, 1080], pos = [0, -2])
 
 def call_texture(stim, setAmp):
     #call c function through command line 
@@ -65,12 +67,16 @@ def call_texture(stim, setAmp):
 
 def message_disp(trial):
     if (trial == 1):
+        win.setColor('blue')
+        win.flip()
         message_stim1_wait.draw()
         win.flip()
         core.wait(1)
         message_stim1.draw()
         win.flip()
     elif (trial == 2):
+        win.setColor('green')
+        win.flip()
         message_stim2_wait.draw()
         win.flip()
         core.wait(1)
@@ -82,18 +88,21 @@ def message_disp(trial):
 
 resp_corr, reversals = [], []
 exit_loop, i = 0, 0
-newAmp = expInfo['starting amplitude']
+newAmp = expInfo['starting amplitude']*2 # newAmp starts greater than the constant amplitude
+#newAmp = expInfo['starting amplitude']/2 # newAmp starts less than the constant amplitude
+
+step_size = [int(newAmp/4), int(newAmp/8),int(newAmp/8), int(newAmp/12), int(newAmp/12), int(newAmp/20)] 
 while exit_loop == 0: 
     
-    step_size = 10
-    
-    diff = abs(expInfo['starting amplitude'] - newAmp)
+    diff = expInfo['starting amplitude'] - newAmp
     
     stim_order = round(numpy.random.random())
     if stim_order == 0:
         firstAmp = expInfo['starting amplitude']
+        secondAmp = newAmp
     else:
         firstAmp = newAmp
+        secondAmp = expInfo['starting amplitude']
     
     message_disp(1)
     call_texture(stim_order, int(newAmp))
@@ -102,9 +111,11 @@ while exit_loop == 0:
     call_texture((not stim_order), int(newAmp))
     
     core.wait(0.5)
-    
-    message_response.draw()
+    win.setColor('grey')
     win.flip()
+    image_response.draw()
+    win.flip()
+    
     #get response
     thisResp=None
     while thisResp==None:
@@ -123,33 +134,46 @@ while exit_loop == 0:
             elif thisKey in ['q', 'escape']:
                 core.quit()#abort experiment
             else:
-                pass
+                thisResp = None 
         event.clearEvents() #must clear other (eg mouse) events - they clog the buffer
+    
+#    print(thisResp)
     
     oldAmp = newAmp
     
-    if ((thisResp == 1) and (firstAmp > expInfo['starting amplitude'])) or ((thisResp == 2) and (firstAmp > expInfo['starting amplitude'])):
-        newAmp = expInfo['starting amplitude']-(diff-step_size) # Correct response - Reduce difference by one step
+    if ((thisResp == 1) and (firstAmp > secondAmp)) or ((thisResp == 2) and (firstAmp < secondAmp)):
+        newAmp -= step_size[len(reversals)] # Correct response - Reduce difference by one step
         resp_corr += [1]
-    elif ((thisResp == 1) and (firstAmp < expInfo['starting amplitude'])) or ((thisResp == 1) and (firstAmp < expInfo['starting amplitude'])):
-        newAmp = expInfo['starting amplitude']-(diff+3*step_size) # Incorrect response - Increase difference by 3 steps
+    elif ((thisResp == 1) and (firstAmp <= secondAmp)) or ((thisResp == 2) and (firstAmp >= secondAmp)):
+        newAmp += 3*step_size[len(reversals)] # Incorrect response - Increase difference by 3 steps
         resp_corr += [0]
     elif (thisResp == 0):
-        newAmp = expInfo['starting amplitude']-(diff+step_size) # Unsure response - Increase difference by 1 step
+        newAmp += step_size[len(reversals)] # Unsure response - Increase difference by 1 step
         resp_corr += [0]
     
-    if (len(resp_corr) > 2):
-        if ((resp_corr[i+1] - resp_corr[i]) != 0):
-            reversals += [i, oldAmp]
+    if (newAmp > 1000):
+        newAmp = 1000
+    elif (newAmp < 0):
+        newAmp = 0
     
-    if (len(reversals) > 3):
+#    print(resp_corr)
+    
+    if (len(resp_corr) > 2):
+        if ((resp_corr[i] - resp_corr[i-1]) != 0):
+            reversals += [[i, oldAmp]]
+    
+    if (len(reversals) >= expInfo['max reversals']):
         exit_loop = 1
+        
     write_list = [newAmp, thisResp, resp_corr[i]]
-    dataFile.write(','.join(str(e) for e in write_list))
+    dataFile.write(','.join(str(e) for e in write_list) + '\n')
     core.wait(1)
     i += 1
-
+#    print(i)
+#    print(str(reversals)+'\n')
 #staircase has ended
+
+data_params = [ 
 dataFile.close()
 
 #give some output to user in the command line in the output window
@@ -159,7 +183,7 @@ print([l[1] for l in reversals])
 print('mean of reversal intensities = ' + str(numpy.average([l[1] for l in reversals])))
 #give some on screen feedback
 feedback1 = visual.TextStim(win, pos=[0,+3],
-    text='mean of final 6 reversals = %.3f' + str(numpy.average([l[1] for l in reversals])))
+    text='mean of final 6 reversals = ' + str(numpy.average([l[1] for l in reversals])))
 feedback1.draw()
 #fixation.draw()
 win.flip()
