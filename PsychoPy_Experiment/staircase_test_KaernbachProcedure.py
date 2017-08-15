c0c37ee52cc47a5b1574d9164479aba7fe97baa3
Miniucnchew <ods7@pitt.edu>
2017-08-15 14:28:52 -0400

    fixed the KaernbachProcedure program.

--- PsychoPy_Experiment/staircase_test_KaernbachProcedure.py
+++ PsychoPy_Experiment/staircase_test_KaernbachProcedure.py
@@ -9,20 +9,19 @@ from subprocess import call
 try:#try to get a previous parameters file
     expInfo = fromFile('TestData/lastParams.pickle')
 except:#if not there then use a default set
-    expInfo = {'observer':'ods7', 'waveform':1, 'frequency':20, 'starting amplitude':300, 'offset':0, 'duration':5, 'max reversals':6}
-expInfo['dateStr']= data.getDateStr() #add the current time
+    expInfo = {'subject ID':'s1_1', 'waveform':1, 'frequency':20, 'starting amplitude':300, 'duration':5, 'max reversals':8}
 #present a dialogue to change params
-dlg = gui.DlgFromDict(expInfo, title='JND Exp', fixed=['dateStr'])
+dlg = gui.DlgFromDict(expInfo, title='JND Exp')
 if dlg.OK:
     toFile('TestData/lastParams.pickle', expInfo)#save params to file for next time
 else:
     core.quit()#the user hit cancel so exit
 
 #make a text file to save data
-fileName = 'TestData/' + expInfo['observer'] + expInfo['dateStr']
-dataFile = open(fileName+'.csv', 'w')#a simple text file with 'comma-separated-values'
-dataFile.write('new_amp,user_resp, resp_correct\n')
-
+fileName = 'TestData/' + expInfo['subject ID'] 
+dataFile = open(fileName+'.txt', 'w')#a simple text file
+dataFile.write('Texture 1 Amp | Texture 2 Amp | User Response | Response Correct? | Reversal Point?\n')
+#                           13            |           13          |           13          |             17               |             15         
 #create window and stimuli
 win = visual.Window([1600,1200],allowGUI=False, monitor='testMonitor', units='cm', fullscr=True)
 win.mouseVisible = False
@@ -52,16 +51,18 @@ message_response = visual.TextStim(win, text = 'If the first texture was stronge
     +' ,if the second texture was stronger, press the right arrow key.'
     +' If you are unsure of which stimulus is stronger, press the down arrow key. \n\n'
     +'If you would like to replay one of the stimuli, press the number key corresponding to the stimulus you would like to return to.')
-image_response = visual.ImageStim(win, image = 'PsychExperiment_KeyboardInstructions.png', units = 'pix', interpolate = True, size = [1440, 1080], pos = [0, -2])
+image_response = visual.ImageStim(win, image = 'PsychExperiment_KeyboardInstructions.png', units = 'pix', interpolate = True)#, size = [1440, 1080]), pos = [0, -2])
+
+offset = 1500
 
 def call_texture(stim, setAmp):
     #call c function through command line 
     if stim == 0:
         call(' '.join(["sudo ./parameter_test", str(expInfo['waveform']), str(expInfo['frequency']),
-        str(expInfo['starting amplitude']), str(expInfo['offset']), str(expInfo['duration'])]), shell=True)
+        str(expInfo['starting amplitude']), str(offset), str(expInfo['duration'])]), shell=True)
     elif stim == 1:
         call(' '.join(["sudo ./parameter_test", str(expInfo['waveform']), str(expInfo['frequency']),
-        str(int(setAmp)), str(expInfo['offset']), str(expInfo['duration'])]), shell=True)
+        str(int(setAmp)), str(offset), str(expInfo['duration'])]), shell=True)
     else:
         pass
 
@@ -90,8 +91,9 @@ resp_corr, reversals = [], []
 exit_loop, i = 0, 0
 newAmp = expInfo['starting amplitude']*2 # newAmp starts greater than the constant amplitude
 #newAmp = expInfo['starting amplitude']/2 # newAmp starts less than the constant amplitude
+response_dict = {0:'Unsure', 1:'1st', 2:'2nd'}
 
-step_size = [int(newAmp/4), int(newAmp/8),int(newAmp/8), int(newAmp/12), int(newAmp/12), int(newAmp/20)] 
+step_size = [int(newAmp*(1./2.)**n) for n in [2,3,3,4,4,5,5,6,6]]
 while exit_loop == 0: 
     
     diff = expInfo['starting amplitude'] - newAmp
@@ -120,6 +122,7 @@ while exit_loop == 0:
     thisResp=None
     while thisResp==None:
         allKeys=event.waitKeys()
+#        allKeys = iohub.client.keyboard.KeyboardPress()
         for thisKey in allKeys:
             if (thisKey == '1'): 
                 call_texture(stim_order, int(newAmp))
@@ -151,29 +154,34 @@ while exit_loop == 0:
         newAmp += step_size[len(reversals)] # Unsure response - Increase difference by 1 step
         resp_corr += [0]
     
-    if (newAmp > 1000):
+    if (newAmp < expInfo['starting amplitude']):
+        newAmp = expInfo['starting amplitude']
+    elif (newAmp > 1000):
         newAmp = 1000
-    elif (newAmp < 0):
-        newAmp = 0
     
 #    print(resp_corr)
-    
+    reversal_point = 'False'
     if (len(resp_corr) > 2):
         if ((resp_corr[i] - resp_corr[i-1]) != 0):
             reversals += [[i, oldAmp]]
+            reversal_point = 'True'
+        else:
+            reversal_point = 'False'
     
     if (len(reversals) >= expInfo['max reversals']):
         exit_loop = 1
         
-    write_list = [newAmp, thisResp, resp_corr[i]]
-    dataFile.write(','.join(str(e) for e in write_list) + '\n')
+    write_list = ['{:^13d}'.format(firstAmp), '{:^13d}'.format(secondAmp), '{:^13}'.format(response_dict[thisResp]), '{:^17d}'.format(resp_corr[i]), '{:^15}'.format(reversal_point)]
+    
+    
+    dataFile.write(' | '.join(str(e) for e in write_list) + '\n')
     core.wait(1)
     i += 1
 #    print(i)
 #    print(str(reversals)+'\n')
 #staircase has ended
 
-data_params = [ 
+#data_params = [ 
 dataFile.close()
 
 #give some output to user in the command line in the output window
